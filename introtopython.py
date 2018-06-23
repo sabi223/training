0e281a49c63d861b3d0e3cbb129eccdc3f19c542
sabi223 <40389310+sabi223@users.noreply.github.com>
2018-06-23 13:20:37 +0530

    Create introtopython.py

---
+++ introtopython.py
@@ -0,0 +1,33 @@
+print("Q1 my name is Sabi Gupta")
+Q1 my name is Sabi Gupta
+
+ print(" Acad"+"View")
+  AcadView
+  
+  print("Q3")
+  x = input("enter 1 first no")
+enter 1 first no 10
+print(x)
+ 10
+y = input("enter 2 no.")
+enter 2 no.20
+ print(y)
+20
+ z = input("enter 3 no")
+enter 3 no30
+ print(z)
+30
+
+print("Q4 Let's get started")
+Q4 Let's get started
+
+s = "acadview"
+course = "python"
+ fees = 5000
+ print("%s,%s,%d"%(s,course,fees))
+acadview,python,5000
+
+name = "Tony Stark"
+salary = 1000000
+print('%s''%d'%(name,salary))
+Tony Stark1000000
